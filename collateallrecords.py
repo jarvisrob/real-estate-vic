6bf4ca2b1dd3e39a7b755024626eba1682086191
Rob Jarvis <jarvisrob@users.noreply.github.com>
2017-07-17 09:17:10 +1000

    collate all records func now put headers in resulting csv

--- collateallrecords.py
+++ collateallrecords.py
@@ -5,6 +5,7 @@ import glob
 files_csv = glob.glob('*.csv')
 
 file_out = open('all_records.csv', 'a')
+file_out.writelines('Suburb, Address, Type, NumberOfBedrooms, Price, Year, Month, Day, Outcome, Agent, Website\n')
 for file_name in files_csv:
     with open(file_name) as file:
         records = file.readlines()
