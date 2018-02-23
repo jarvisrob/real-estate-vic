75940b36f9e491d21b7a090007c55715325272ce
Rob Jarvis <jarvisrob@users.noreply.github.com>
2018-02-23 16:52:45 +1100

    added sql for setting up database

---
+++ sql/db_setup.sql
@@ -0,0 +1,56 @@
+/* ---------------------------------------------------
+SET-UP: DROP EVERYTHING THEN CREATE SCHEMAS AND TABLES
+------------------------------------------------------
+This is SQL code for setting up the database.
+It creates the schema(s) and tables, first dropping
+them if they already exist.
+WARNING: Running this will delete/drop any data that
+currently exists in teh database! This is intended for
+set-up only (or a complete restart).
+-----------------------------------------------------*/
+
+-- Make sure using the RealEstateVicDb database
+USE RealEstateVicDb;
+GO
+
+-- Kill any existing schemas or tables if exists
+DROP TABLE IF EXISTS RealEstate.Results;
+DROP TABLE IF EXISTS RealEstate.PrelimResults;
+DROP SCHEMA IF EXISTS RealEstate;
+GO
+
+-- Create ReslEstate schema
+CREATE SCHEMA RealEstate;
+GO
+
+-- Main results table: RealEstate.Results
+CREATE TABLE RealEstate.Results
+(
+	ResultId			INT IDENTITY(1,1) PRIMARY KEY,
+	Suburb				NVARCHAR(50) NOT NULL,
+	AddressLine			NVARCHAR(80) NOT NULL,
+	Classification		NVARCHAR(80) NOT NULL,
+	NumberOfBedrooms	TINYINT NULL,
+	Price				INT NULL,
+	OutcomeDate			DATE NOT NULL,
+	Outcome				NVARCHAR(50) NOT NULL,
+	Agent				NVARCHAR(100) NULL,
+	WebUrl				NVARCHAR(200) NULL
+);
+GO
+
+-- Prelim table for results early in week: RealEstate.PrelimResults
+-- Created as copy of main results table, WHERE 1 = 2 ensures no rows are copied, just column names
+SELECT *
+INTO RealEstate.PrelimResults
+FROM RealEstate.Results
+WHERE 1 = 2;
+GO
+
+-- Rename the ID column in Prelim table and then make it the primary key
+EXEC sys.sp_rename 'RealEstate.PrelimResults.ResultId', 'PrelimResultId', 'COLUMN';
+GO
+ALTER TABLE RealEstate.PrelimResults
+ADD CONSTRAINT PK_PrelimResultId PRIMARY KEY (PrelimResultId);
+GO
+
